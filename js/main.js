1e5cdad9f519ea13f52710caa9eebf21bb952a3a
Luis <luis.rodriguezcastro31@gmail.com>
2018-06-02 07:23:22 -0700

    some comments to the code

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
 window.onload = () => {
+
     //Creating the table
     var bdmsTicTac = [];
     let createBoard = (dimension) => {
-        let tr;
-        let td;
-        let table = document.getElementById('tictactoe');
-        while (table.firstChild) {
-            table.removeChild(table.firstChild)
-        }
-        for (let trIndex = 1; trIndex <= dimension; ++trIndex) {
-            tr = document.createElement("tr");
-            tr.id = trIndex;
-            for (let tdIndex = 1; tdIndex <= dimension; ++tdIndex) {
-                td = document.createElement("td");
-                td.id = `${trIndex}-${tdIndex}`;
-                td.className = `${trIndex}-td-${tdIndex}`;
-                tr.appendChild(td)
+        if (dimension > 15) {
+            alert(`Board is larger than 15 don't do that...`);
+        } else {
+            let tr;
+            let td;
+            let table = document.getElementById('tictactoe');
+            while (table.firstChild) {
+                table.removeChild(table.firstChild)
             }
-            table.appendChild(tr);
+            for (let trIndex = 1; trIndex <= dimension; ++trIndex) {
+                tr = document.createElement("tr");
+                tr.id = trIndex;
+                for (let tdIndex = 1; tdIndex <= dimension; ++tdIndex) {
+                    td = document.createElement("td");
+                    td.id = `${trIndex}-${tdIndex}`;
+                    td.className = `${trIndex}-td-${tdIndex}`;
+                    tr.appendChild(td)
+                }
+                table.appendChild(tr);
+            }
+            listenerToEachTd();
         }
-        listenerToEachTd();
     }
+
+    //Showing the board based on the user input
+    let showBoard = () => {
+        document.getElementById('generateButton')
+            .addEventListener("click", function () {
+                let dimension = document.getElementById("dimension").value;
+                createBoard(Number(dimension));
+            });
+    }
+
     //Click event to each cell in the table
     let listenerToEachTd = () => {
         let boolSwitcher = false;
@@ -30,32 +45,33 @@ window.onload = () => {
                     boolSwitcher = !boolSwitcher;
                     if (this.innerText === '') {
                         boolSwitcher === true ? this.innerText = "O" : this.innerText = "X";
+                        boardToArray();
+                        bdmsTicTac = [];
                     }
-                    boardToArray();
-                    bdmsTicTac = [];
                 }))
-    };
-
-    //Showing the board based on the user input
-    let showBoard = () => {
-        document.getElementById('generateButton')
-            .addEventListener("click", function () {
-                let clicking = document.getElementById("dimension").value;
-                createBoard(Number(clicking));
-            });
     }
-    //Parse board into bidimensional array
+
+    //Parsing the board into multidimensional array
     let boardToArray = () => {
         let tds = document.querySelectorAll("#tictactoe td");
         let count = 0;
         var tempTds = '';
         for (let td = 0; td < tds.length; ++td) {
-            tds[td].innerHTML == '' ? tempTds += " " : tempTds += tds[td].innerHTML;
+            //Ternary conditions, making sure that the array will be able to have empty values(non-clicked cells in the board)
+            tds[td].innerHTML == '' ? tempTds += ' ' : tempTds += tds[td].innerHTML;
+
+            //Counter to check everytime the loop increments to the given dimension
             ++count;
-            count.toString() == document.getElementById('dimension').value ?
-                (bdmsTicTac.push(tempTds.split('')), tempTds = '', count = 0) : count;
+
+            //Ternary condition, if the counter is equal the dimension
+            count.toString() === document.getElementById('dimension').value ?
+                (bdmsTicTac.push(tempTds.split('')),
+                    tempTds = '',
+                    count = 0) : count;
         }
         console.log(bdmsTicTac);
     }
+
+
     showBoard();
 }
